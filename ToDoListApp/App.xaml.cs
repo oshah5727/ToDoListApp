dd0e50903555779caabdd6e5e94e5a827fcc4108
Opal Shah <shahol@mail.uc.edu>
2025-04-09 09:48:33 -0400

    fixed errors in App.xaml.cs

--- ToDoListApp/App.xaml.cs
+++ ToDoListApp/App.xaml.cs
@@ -6,7 +6,7 @@
         {
             InitializeComponent();
 
-            MainPage = new NavigationPage(LoginPage);
+            MainPage = new NavigationPage(new LoginPage());
         }
     }
 }
