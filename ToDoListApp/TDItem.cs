6ff288186a7850ddf638a24696b1af0016fa6f04
Ethan Goudy <tesladyne56@gmail.com>
2025-04-27 17:46:33 -0400

    Restored database configuration

--- ToDoListApp/TDItem.cs
+++ ToDoListApp/TDItem.cs
@@ -9,12 +9,16 @@ namespace ToDoListApp
 {
     public class TDItem
     {
-        [PrimaryKey, AutoIncrement]
+        [PrimaryKey, AutoIncrement, Column("id")]
         public int ID { get; set; }
 
+        [Column("name")]
         public string Name { get; set; }
+       
+        [Column("description")]
         public string Description { get; set; }
 
+        [Column("date")]
         public string Date { get; set; }
     }
 }
