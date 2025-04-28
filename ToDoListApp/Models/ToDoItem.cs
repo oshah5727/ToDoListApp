0cd013de93321792108db971bae69cfe080e7b71
Ethan Goudy <tesladyne56@gmail.com>
2025-04-28 15:43:42 -0400

    Buttons are now clickable, but not in bar layout

--- ToDoListApp/Models/ToDoItem.cs
+++ ToDoListApp/Models/ToDoItem.cs
@@ -3,14 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SQLite;
 
 namespace ToDoListApp.Models
 {
+    [Table("TDItem")]
     public class ToDoItem
     {
+        [PrimaryKey]
+        [AutoIncrement]
+        [Column("id") ]
+        public int ID { get; set; }
+        [Column("name")]
         public string Name { get; set; }
+        [Column("description")]
         public string Description { get; set; }
+        [Column("due_by")]
         public string DueBy { get; set; }
+        [Column("is_complete")]
         public bool IsComplete { get; set; }
 
     }
